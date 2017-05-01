7b7f0640d8f3cd43e033b041d64673473786b902
ugurasaner <ugurasaner@gmail.com>
2017-05-01 22:45:03 +0300

    test scripts

--- raspapp/libraries/sensor/test.py
+++ raspapp/libraries/sensor/test.py
@@ -7,9 +7,9 @@ while 1:
     try:
 
         print 'Water:' + str(instance.do_scale(27, 17, 1))
-        time.sleep(.5)
+
         print 'Food:' + str(instance.do_scale(26, 19, 1))
-        time.sleep(.5)
+
 
     except KeyboardInterrupt:
         sys.exit()
