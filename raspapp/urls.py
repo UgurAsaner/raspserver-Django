5cc9dc70e422201adbcf6d3035fa2f74e1a95e23
ugurasaner <ugurasaner@gmail.com>
2017-05-01 15:48:11 +0300

    demo version

--- raspapp/urls.py
+++ raspapp/urls.py
@@ -5,7 +5,9 @@ from raspapp import views
 
 urlpatterns = [
 
-    url(r'test/', views.Test.as_view())
+    url(r'test/', views.Test.as_view()),
+    url(r'food/', views.food.as_view()),
+    url(r'water/', views.water.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- raspapp/views.py
+++ raspapp/views.py
@@ -1,6 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from raspapp.controllers import sensorController
 from raspapp.cronjobs import functions
 
 
@@ -9,5 +10,18 @@ class Test(APIView):
     def get(self, request):
 
         return Response(functions.send_data())
+
+class food(APIView):
+
+    def get(self, request):
+
+        return Response(sensorController.get_food_amount())
+
+class water(APIView):
+
+    def get(self, request):
+
+        return Response(sensorController.get_water_amount())
+
 
 
