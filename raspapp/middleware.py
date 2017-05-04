87712bf45715213be287a8f2061fa4e0cc677cfd
ugurasaner <ugurasaner@gmail.com>
2017-05-05 01:36:27 +0300

    middleware configuration

--- raspapp/middleware.py
+++ raspapp/middleware.py
@@ -1,15 +1,21 @@
+from rest_framework.response import Response
+
+from models import Server
 
 class Middleware(object):
 
     def __init__(self, get_response):
         self.get_response = get_response
 
-    def process_request(self, request):
+    def __call__(self, request):
 
         request_ip = request.META['REMOTE_ADDR']
+        allowed_ip = Server.objects.get(name='piserver').ip
 
-        return request_ip
+        if request_ip is not allowed_ip:
+            return Response('Unauthorized')
 
-        # response = self.get_response(request)
-        #
-        # return response
+        response = self.get_response(request)
+        return response
+
+
