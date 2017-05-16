52885c26c7df37e713c4f7cb6c5554e1b672050b
ugurasaner <ugurasaner@gmail.com>
2017-05-16 18:31:21 +0300

    full test

--- raspapp/views.py
+++ raspapp/views.py
@@ -1,13 +1,17 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from raspapp.controllers import amountController, foodController, waterController
+from raspapp.models import Server
 
 
 class Test(APIView):
 
     def get(self, request):
 
-        return Response(True)
+        request_ip = request.META['REMOTE_ADDR']
+        allowed_ip = Server.objects.get(name='piserver').ip
+
+        return Response("r: "+request_ip + "\na: " + allowed_ip)
 
 class food(APIView):
 
